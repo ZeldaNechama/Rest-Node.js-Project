ee271f0cd34aa8052411c42e45e8f1efdd3ce785
ZeldaNechama <zeldanechama@gmail.com>
2024-06-10 16:13:29 +0300

    addes swagger ability

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express from 'express';
-import busineesRouter from "./routes/business.routes"
+import bodyParser from "body-parser";
+import busineesRouter from "./routes/business.routes";
+import serviceRouter from './routes/services.routes';
+import meetingRouter from './routes/meetings.routes';
+import userRouter from './routes/users.routes';
 import errorHandler from './middlewares/errorHandler';
+import connectDB from './database';
 
 const app = express();
+connectDB();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/business',busineesRouter);
+app.use('/api/service',serviceRouter);
+app.use('/api/meeting',meetingRouter);
+app.use('/api/user',userRouter);
 app.use(errorHandler);
 
 export default app;
