a745c22c5471e353b4f78d6444397d5ccc8d9bcf
ZeldaNechama <zeldanechama@gmail.com>
2024-06-21 12:36:21 +0300

    connection to DB

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log('MONGODB_URI:', process.env.MONGODB_URI); // הוסף שורה זו
+console.log('MONGODB_URI:', process.env.MONGODB_URI!); 
 
 const connectDB = async () => {
   try {
-   await mongoose.connect(process.env.MONGODB_URI!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongoose.ConnectOptions);
+    mongoose.connect(process.env.MONGODB_URI!);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err);
